ca352789ea0ca8261586e5246067e4d0b072fdfe
pvpatil5 <pvpatil123@yahoo.com>
2021-04-22 13:37:57 +0530

    Secod Commit

--- src/MapKeySet.java
+++ src/MapKeySet.java
@@ -7,10 +7,7 @@ import java.util.Set;
 public class MapKeySet {
 	public static void main(String[] args) {
 		HashMap<Integer, String> map=new HashMap<Integer, String>();
-		map.put(1, "One");
-		map.put(2, "Two");
-		map.put(3, "Three");
-		map.put(4, "Four");
+		
 		map.put(5, "Five");
 		map.put(6, "Six");
 		map.put(7, "Seven");
